e3af7c226d2810b5c2d4dfa60602b41deb2a4f22
Arsh  Mittal <arshmittal451@gmail.com>
2025-04-07 21:25:57 +0530

    test location

--- src/common/services/base.service.ts
+++ src/common/services/base.service.ts
@@ -64,7 +64,7 @@ export class BaseService {
         return data.results[0].formatted_address;
       }
   
-      return 'Unknown Location';
+      return 'Unknown Locations';
     } catch (error) {
       console.error('Fetch error:', error.message || error);
       return 'Unknown Location';
